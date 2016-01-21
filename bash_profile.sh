7b904d926a505ce84671de7f56447b382938b9c1
Jeremy Anderson <Jeremy.Anderson@macys.com>
2016-01-21 10:25:50 -0700

    final

--- bash_profile.sh
+++ bash_profile.sh
@@ -12,7 +12,6 @@ export CODESIGN_ALLOCATE="/Applications/Xcode.app/Contents/Developer/Toolchains/
 test -f ~/.bashrc && source ~/.bashrc
 #
 # Load RVM into a shell session *as a function*
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 #
 export M2_HOME=`brew --prefix maven`/libexec
 export M2=$M2_HOME/bin
@@ -27,6 +26,7 @@ export PATH
 export DYLD_LIBRARY_PATH=/opt/oracle/instantclient_11_2:$DYLD_LIBRARY_PATH
 export PATH=/opt/oracle/instantclient_11_2:$PATH
 export ORACLE_HOME=/opt/oracle/instantclient_11_2
+export OCI_DIR=$ORACLE_HOME
 #
 # db2 path stuff
 DB2HOME="/Users/db2inst1/sqllib"
@@ -38,14 +38,22 @@ export DOCKER_HOST=tcp://192.168.59.103:2376
 export DOCKER_CERT_PATH=$HOME/.boot2docker/certs/boot2docker-vm
 export DOCKER_TLS_VERIFY=1
 #
+# Load RVM into a shell session *as a function*
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+#
 # Alias Helpers
- alias qaa='cd ~/git/QAA'
- alias ios='cd ~/git/ios-app'
- alias bp='cat ~/.bash_profile'
- alias ebp='vim '$HOME'/.bash_profile'
- alias build_mcom='update_app update mcom qa weak'
- alias build_bcom='update_app update bcom qa weak'
- alias mcom_irb="CALABASH_FULL_CONSOLE_OUTPUT=1 APP_BUNDLE_PATH=$HOME'/git/QAA/test_servers/MCOM QA.app' DEVICE_TARGET=simulator DEVICE_ENDPOINT='http://localhost:37265' DEBUG=1 calabash-ios console"
- alias start_mcom="echo 'start_test_server_in_background' | mcom_irb && mcom_irb"
- alias bcom_irb="CALABASH_FULL_CONSOLE_OUTPUT=1 APP_BUNDLE_PATH=$HOME'/git/QAA/test_servers/BCOM QA.app' DEVICE_TARGET=simulator DEVICE_ENDPOINT='http://localhost:37265' DEBUG=1 calabash-ios console"
- alias start_bcom="echo 'start_test_server_in_background' | bcom_irb && bcom_irb"
+alias qaa='cd ~/git/QAA'
+alias ios='cd ~/git/ios-app'
+alias bp='cat ~/.bash_profile'
+alias ebp='vim '$HOME'/.bash_profile'
+alias build_mcom='build_app mcom update'
+alias build_bcom='build_app bcom update'
+alias code='cd ~/Code'
+alias mcom_irb="CALABASH_FULL_CONSOLE_OUTPUT=1 APP_BUNDLE_PATH=$HOME'/git/QAA/test_servers/MCOM QA.app' DEVICE_TARGET=simulator DEVICE_ENDPOINT='http://localhost:37265' DEBUG=1 calabash-ios console"
+alias start_mcom="echo 'start_test_server_in_background' | mcom_irb && mcom_irb"
+alias bcom_irb="CALABASH_FULL_CONSOLE_OUTPUT=1 APP_BUNDLE_PATH=$HOME'/git/QAA/test_servers/BCOM QA.app' DEVICE_TARGET=simulator DEVICE_ENDPOINT='http://localhost:37265' DEBUG=1 calabash-ios console"
+alias start_bcom="echo 'start_test_server_in_background' | bcom_irb && bcom_irb"
+alias glog='git log --oneline --decorate --all --graph'
+alias rcop='rake app:ios:rubocop'
+alias push='git-review'
+
--- setup_script.sh
+++ setup_script.sh
@@ -8,14 +8,18 @@ mag=$'\e[35m'
 cyn=$'\e[36m'
 end=$'\e[0m'
 ###############################################################################
-# A shell script to help QE folk set up their Mac development environment.    #
-# An attempt to shorten the cycle between new-hire and useful asset.          #
+# A shell script to help QE folk set up their Mac development environment.
+# An attempt to shorten the cycle between new-hire and useful asset.
+# If someone wanted to iterate on this and refine it, it might make them look
+# like a star.
+#
+# Cheers to you all,
+# Jeremy Anderson
 ###############################################################################
 #
 ###############################################################################
 # Interactively sets values for these variables before running this script    #
-# Delete your password from this file when done so you don't get compromised. #
-##########
+###############################################################################
 #
 echo -en "
         ${blu}This script will attempt to automate the installation and configuration
@@ -34,34 +38,34 @@ GERRIT_HOST="qagerrit"
 ###############################################################################
 # Write .bash_profile and create setup.log                                                         #
 ###############################################################################
-touch ./setup.log && LOG="setup.log" && cp ./bash_profile.sh ~/.bash_profile || echo "failed to write .bash_profile" >> $LOG ;
+touch ./setup.log && LOG="setup.log" && cp ./bash_profile.sh ~/.bash_profile || echo "failed to write .bash_profile" >> ${LOG} ;
 ###############################################################################
 # Install Essential Dependencies                                              #
 ###############################################################################
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && brew update && brew doctor || echo Homebrew installation FAILED >> $LOG &&
-brew install git || echo Failed Git install is fail. >> $LOG;
-curl -sSL https://get.rvm.io | bash -s stable || echo RVM install has failed. >> $LOG;
-rvm install ruby-2.0.0-p643 || echo Failed to install Ruby 2.0.0 >> $LOG;
-brew install python || echo Failed Python installation is fail. >> $LOG;
-echo $PASSWORD | sudo -S pip install git-review || echo Failed Git-Reivew installation via pip. >> $LOG;
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && brew update && brew doctor || echo Homebrew installation FAILED >> ${LOG} &&
+brew install git || echo Failed Git install is fail. >> ${LOG};
+curl -sSL https://get.rvm.io | bash -s stable || echo RVM install has failed. >> ${LOG};
+rvm install ruby-2.0.0-p643 || echo Failed to install Ruby 2.0.0 >> ${LOG};
+brew install python || echo Failed Python installation is fail. >> ${LOG};
+echo ${PASSWORD} | sudo -S pip install git-review || echo Failed Git-Reivew installation via pip. >> ${LOG};
 ###############################################################################
 # Configure $PATH variable                                                    #
 ###############################################################################
-export PATH=/"/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bash_profile || echo 'failed to write new path paths to $PATH' >> $LOG;
+export PATH=/"/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bash_profile || echo 'failed to write new path paths to $PATH' >> ${LOG};
 ###############################################################################
 # Configure RVM/Ruby                                                          #
 ###############################################################################
-rvm use 2.0.0 --default || echo 'set Ruby 2.0.0 as rvm default FAILED' >> $LOG;
+rvm use 2.0.0 --default || echo 'set Ruby 2.0.0 as rvm default FAILED' >> ${LOG};
 ###############################################################################
 # Configure Git globals and directory for repos
 # Clone Git repositories for QAA and Gerrit
 ###############################################################################
 mkdir ~/git
-git config --global user.name $HUMAN_NAME
-git config --global user.email Jeremy.Anderson@macys.com
+git config --global user.name ${HUMAN_NAME}
+git config --global user.email ${HUMAN_NAME/ /.}@macys.com
 git config --global merge.tool kdiff3
-git config --global --add gitreview.username $RACFID
-echo "HERE IS YOUR GLOBAL GIT CONFIG:" >> $LOG && git config --global --list >> $LOG;
+git config --global --add gitreview.username ${RACFID}
+echo "HERE IS YOUR GLOBAL GIT CONFIG:" >> ${LOG} && git config --global --list >> ${LOG};
 ###############################################################################
 # Generate SSH Key                                                            #
 ###############################################################################
@@ -69,21 +73,21 @@ ssh-keygen -t rsa -N ""
 ###############################################################################
 # Configure Pre-commit hook for Gerrit                                        #
 ###############################################################################
-scp -p -P 29418 $RACFID@qagerrit:hooks/commit-msg ~/git/QAA/.git/hooks/commit-msg || echo "failed to write git pre-commit message hook" >> $LOG;
+scp -p -P 29418 $RACFID@qagerrit:hooks/commit-msg ~/git/QAA/.git/hooks/commit-msg || echo "failed to write git pre-commit message hook" >> ${LOG};
 ###############################################################################
 # Install and Configure Gems                                                  #
 ###############################################################################
-gem install bundler || echo "Bundler Gem install failed." >> $LOG;
-gem install calabash-cucumber || echo "Calabash Gem failed to install" >> $LOG;
+gem install bundler || echo "Bundler Gem install failed." >> ${LOG};
+gem install calabash-cucumber || echo "Calabash Gem failed to install" >> ${LOG};
 ###############################################################################
 # Accept XCode License Agreement (or else!)                                   #
 ###############################################################################
-echo $PASSWORD | sudo -S /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -license accept || echo "XCode license agreement NOT accepted." >> $LOG;
+echo $PASSWORD | sudo -S /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -license accept || echo "XCode license agreement NOT accepted." >> ${LOG};
 ###############################################################################
 # Mount Share and run Ruby 2 DB Setup script.                                 #
 ###############################################################################
 # @TODO: Figure out Samba Mounting Command for
-open smb://$RACFID:$LOCAL_PASSSWORD@mcwgroups/mdc-document || echo "Samba Mount Failed" >> $LOG;
+open smb://$RACFID:$LOCAL_PASSSWORD@mcwgroups/mdc-document || echo "Samba Mount Failed" >> ${LOG};
 ###############################################################################
 # create or edit /etc/sysctl.conf
 ###############################################################################
@@ -93,9 +97,12 @@ kern.sysv.shmmni=4096
 kern.sysv.shmseg=32
 kern.sysv.shmall=1179648
 kern.sysv.maxfilesperproc=65536
-kern.sysv.maxfiles=65536" >> /etc/sysctl.conf || echo "Failed to write /etc/sysctl.conf" >> $LOG;
+kern.sysv.maxfiles=65536" >> /etc/sysctl.conf || echo "Failed to write /etc/sysctl.conf" >> ${LOG};
 ###############################################################################
 
 cp "/Volumes/mdc-document/QAA team Setup Installers/Ruby200Upgrade/mac/db2_v101_macos_expc.tar.gz" ~/
-tar -xvf ~/db_2v101_macosx_expc.tar.gz || echo "extract of db2 installer failed" >> $LOG;
+tar -xvf ~/db_2v101_macosx_expc.tar.gz || echo "extract of db2 installer failed" >> ${LOG};
+
+# There's other stuff to do, but certainly this covers a lot of it.
+# Not sure if valid?
 #cd ~/expc && sudo ./db2_install || echo "failed to install db2";
